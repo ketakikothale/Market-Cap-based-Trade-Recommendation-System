d928d359bcfeecbb9dff6b514fb3cf5bc52cb3d5
Chetana Nagare <82255820+chetana074@users.noreply.github.com>
2022-09-21 11:20:24 +0530

    Add files via upload

--- MarketCap/src/main/java/com/citi/marketcap/dto/Stock.java
+++ MarketCap/src/main/java/com/citi/marketcap/dto/Stock.java
@@ -1,5 +1,8 @@
 package com.citi.marketcap.dto;
 
+import java.sql.Date;
+import java.sql.Time;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,30 +14,12 @@ import lombok.NoArgsConstructor;
 public class Stock
 {
 	String symbol;
-//	double change;
 	String name;
 	double price;
 	double beta;
 	double marketCap;
-
-//	double changesPercentage;
-//	double change;
-//	double dayLow;
-//	double dayHigh;
-//	double yearHigh;
-//	double yearLow;
-
-//	double priceAvg50;
-//	double priceAvg200;
 	double volume;
-//	double avgVolume;
-//	String exchange;
-//	double open;
-//	double previousClose;
-//	double eps;
-//	double pe;
-//	String earningsAnnouncement;
-//	long sharesOutstanding;
-//	long timestamp;
 	boolean isActivelyTrading;
+	String date;
+	String time;
 }
