f77e9697bdb5d96c8bc95a62dbc76720ba297da8
Chetana Nagare <82255820+chetana074@users.noreply.github.com>
2022-09-21 11:18:43 +0530

    Add files via upload

--- MarketCap/src/main/java/com/citi/marketcap/MarketCapApplication.java
+++ MarketCap/src/main/java/com/citi/marketcap/MarketCapApplication.java
@@ -14,14 +14,3 @@ public class MarketCapApplication
 		SpringApplication.run(MarketCapApplication.class, args);
 	}
 }
-
-/*
- * 
- * { "symbol" : "FB", "companyName" : "Meta Platforms, Inc.", "marketCap" :
- * 547005300736, "sector" : "Communication Services", "industry" :
- * "Internet Content & Information", "beta" : 1.3761, "price" : 196.6400,
- * "lastAnnualDividend" : 0.0000, "volume" : 31344400, "exchange" :
- * "NASDAQ Global Select", "exchangeShortName" : "NASDAQ", "country" : "US",
- * "isEtf" : false, "isActivelyTrading" : false }
- * 
- */
